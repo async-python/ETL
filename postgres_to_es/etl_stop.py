e649b2fe236dd20d99c4393f97cce16c9a60e391
vardeath <vardeath@yandex.ru>
2022-02-04 16:21:01 +0300

    added model genre and person

--- postgres_to_es/etl_stop.py
+++
@@ -1,5 +0,0 @@
-from redis_base import ProcessStates, RedisState, RedisStorage
-
-if __name__ == '__main__':
-    redis_base = RedisState(RedisStorage())
-    redis_base.set_process_state(ProcessStates.stop)
